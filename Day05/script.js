20a753e503274c76144e45b7858cdb9e466d9939
Taeho0927 <105978681+Taeho0927@users.noreply.github.com>
2022-07-30 17:52:04 +0900

    Add files via upload

---
+++ Day05/script.js
@@ -0,0 +1,15 @@
+/*
+    Author : Taeho GIL
+    Content : Mouse Event
+    Date : 2022. 7. 30.
+*/
+
+function over(arg){
+    arg.src="image/test.jpg";
+}
+function out(arg){
+    arg.src="image/birdTest.jpg";
+}
+function dbclick(arg){
+    arg.src="image/ori.jpg";
+}
